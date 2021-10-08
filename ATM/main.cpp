994ec53c4121bd04890da717d9ebaf781f4143e4
EscapeFromJava <reske.artur@gmail.com>
2021-10-08 16:41:20 +0300

    ver 1.1

--- ATM/main.cpp
+++ ATM/main.cpp
@@ -5,12 +5,14 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	int arr[] = { 1,1,0,9,10,2 }; //массив, где в [0] хранятся купюры по 100 рублей, [1] - по 200 рублей и т.д.
+	int arr[] = { 0,0,0,5,5,0 }; //массив, где в [0] хранятся купюры по 100 рублей, [1] - по 200 рублей и т.д.
 	int arrMoney[] = { 5000,2000,1000,500,200,100 }; //массив с номиналом купюр, начиная с 5000 и на убывание
 	int arrCash[] = { 0,0,0,0,0,0 };
-	int cash, cashF, summa;
-	summa = arr[0] * 5000 + arr[1] * 2000 + arr[2] * 1000 + arr[3] * 500 + arr[4] * 200 + arr[5] * 100;
+	int cash, cashF, summa = 0;
 	for (int i = 0; i < 6; i++) {
+		summa += arr[i] * arrMoney[i];
+	}
+	for (int i = 0; i < 6; i++) {//цикл на вывод количество купюр каждого номинала в банкомате
 		cout << "В банкомате: " << arr[i] << " ед. по " << arrMoney[i] << " рублей" << endl;
 	}
 	cout << "Всего в банкомате " << summa << " рублей" << endl;
@@ -22,67 +24,21 @@ int main()
 		cout << "Банкомат не может выдать данную сумму" << endl;
 		return 0;
 	}
-	if (cash >= 5000) { //проверка на операцию больше или равную 5000
-		if (cash / 5000 <= arr[0]) { //если операция укладывается в лимит элемента массива, то производит дальнейший расчет
-			arrCash[] = cash / 5000;
-			arr[0] -= arrCash[0];
-			cash = cash - arrCash[0] * 5000;
-		}
-		else { //если операция превышает лимит элемента массива, то пересчитываем запрашиваеммую цифру и присваиваем массиву 0
-			cash = cash - arr[0] * 5000;
-			arrCash[0] += arr[0];
-			arr[0] = 0;
-		}
-	}
-	if (cash >= 2000) {
-		if (cash / 2000 <= arr[1]) {
-			arrCash[1] = cash / 2000;
-			arr[1] -= arrCash[1];
-			cash = cash - arrCash[1] * 2000;
-		}
-		else {
-			cash = cash - arr[1] * 2000;
-			arrCash[1] += arr[1];
-			arr[1] = 0;
+	for (int i = 0; i < 5; i++) { //цикл на промежуток от 5000 до 200 купюр
+		if (cash >= arrMoney[i]) { //проверка на операцию больше или равную 5000
+			if (cash / arrMoney[i] <= arr[i]) { //если операция укладывается в лимит элемента массива, то производит дальнейший расчет
+				arrCash[i] = cash / arrMoney[i];
+				arr[i] -= arrCash[i];
+				cash -= arrCash[i] * arrMoney[i];
+			}
+			else { //если операция превышает лимит элемента массива, то пересчитываем запрашиваеммую цифру и присваиваем массиву 0
+				cash -= arr[i] * arrMoney[i];
+				arrCash[i] = arr[i];
+				arr[i] = 0;
+			}
 		}
 	}
-	if (cash >= 1000) {
-		if (cash / 1000 <= arr[2]) {
-			arrCash[2] = cash / 1000;
-			arr[2] -= arrCash[2];
-			cash = cash - arrCash[2] * 1000;
-		}
-		else {
-			cash = cash - arr[2] * 1000;
-			arrCash[2] += arr[2];
-			arr[2] = 0;
-		}
-	}
-	if (cash >= 500) {
-		if (cash / 500 <= arr[3]) {
-			arrCash[3] = cash / 500;
-			arr[3] -= arrCash[3];
-			cash = cash - arrCash[3] * 500;
-		}
-		else {
-			cash = cash - arr[3] * 500;
-			arrCash[3] += arr[3];
-			arr[3] = 0;
-		}
-	}
-	if (cash >= 200) {
-		if (cash / 200 <= arr[4]) {
-			arrCash[4] = cash / 200;
-			arr[4] -= arrCash[4];
-			cash = cash - arrCash[4] * 200;
-		}
-		else {
-			cash = cash - arr[4] * 200;
-			arrCash[4] += arr[4];
-			arr[4] = 0;
-		}
-	}
-	if (cash >= 100) {
+	if (cash >= 100) { //условие для 100 рублевок
 		if (cash / 100 <= arr[5]) {
 			arrCash[5] = cash / 100;
 			arr[5] -= arrCash[5];
@@ -93,6 +49,7 @@ int main()
 			return 0;
 		}
 	}
+	
 	if (cash == 0) {
 		cout << "Операция выдачи наличных выполнена успешно!" << endl;
 	}
@@ -102,6 +59,6 @@ int main()
 	for (int i = 0; i < 6; i++) {
 		cout << "Остаток в банкомате: " << arr[i] << " ед. по " << arrMoney[i] << " рублей" << endl;
 	}
-	cout << "Всего в банкомате осталось: " << summa-cashF << " рублей" << endl;
+	cout << "Всего в банкомате осталось: " << summa - cashF << " рублей" << endl;
 	return 0;
 }
